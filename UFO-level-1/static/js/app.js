e7d2a4cc4222d17098723a738eabd1ba665a164a
airman89 <52982547+ErinMandell@users.noreply.github.com>
2020-01-01 19:46:04 -0600

    moved index.html file and updated js

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,5 +1,6 @@
 // from data.js
 var tableData = data;
+var tbody = d3.select("tbody");
 
 // Select and set a handle on the datetime button
 var button = d3.select("#filter-btn");
@@ -8,5 +9,17 @@ var button = d3.select("#filter-btn");
 
 button.on("click", function() {
     var dateTime = d3.select("#datetime").property("value");
+    console.log(tableData);
     console.log(dateTime);
+
+    var filteredData = tableData.filter(ufo => ufo.datetime === dateTime);
+    console.log(filteredData);
+
+    filteredData.forEach((ufoSighting) => {
+        var row = tbody.append("tr");
+        Object.entries(ufoSighting).forEach(([key,value]) => {
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
 });
