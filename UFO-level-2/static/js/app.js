cfb61dd048b57d1be2ba3a7d57121b0f25561edc
ErinMandell <ErinMandell@users.noreply.github.com>
2020-01-07 14:28:49 -0600

    changed comments in app.py

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -39,7 +39,8 @@ function buildTable(data) {
 // Log user input value and id to verify them
 //
 // Create 'filters' list
-//     - if the input value is truthy, add to filters dictionary and log it,
+//     - if 'inputValue' is Truthy, add to userFilters object with 'filterID' as its key
+//         as in: 'filterID: inputValue', or 'datetime: 1/1/2020' for example, and log it,
 //       else delete the input value.  This 'filters' dictionary is a list of 
 //       all of the key:value pairs of the 'id:value' currently in the input form 
 // 
@@ -66,18 +67,27 @@ function setFilters() {
 
 
 // *** FUNCTION TO FILTER THE DATA SET ***
-// Move the full data set to a new variable to work with it
-// From the 'filters' object, for each key:value pair in that dictionary
+// Move the full data set to a new variable 'filteredData' to work with it.
+// From the 'filters' object built above, with each key:value pair in that dictionary
 // filter the dataset rows where the ??? key???  is equal to the 'filters' value?
-//    ?? I'm not completely clear on why this line of code (#80) works ??
+//    ?? I'm not completely clear on why this line of code (#86) works ??
+//    ?? It looks to me like I would read line 80 as:
+//        "filter the data, select each row where the value of userFilter --- the key of that row"
+//        
+//        I know that it's comparing key:key and value:value, but I'm having trouble reading this symtax
 //
 // Call the buildTable function above to build a new table with this filtered data set
 
 function filterTable() {
     let filteredData = tableData;
     console.log(filteredData);
-    Object.entries(userFilters).forEach(([key, value]) => {
-        filteredData = filteredData.filter((row) => row[key] === value);
+
+    Object.entries(userFilters).forEach(([key1, value2]) => {
+        filteredData = filteredData.filter((row3) => row3[key1] === value2);
+
+        console.log(userFilters);
+        console.log(filteredData);
     });
+
     buildTable(filteredData);
 }
